7415b4b12b7ca93de90cdfa4812e18016b0d2a9a
jarrod henderson <jarrodhenderson@jarrods-MacBook-Pro.local>
2026-07-23 10:38:43 +0200

    add route implemented, writing to json file

--- app.py
+++ app.py
@@ -1,15 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import json
 
 app = Flask(__name__)
 
-with open('blog_posts.json') as file:
-    blog_posts = json.load(file)
+with open('blog_posts.json', 'r', encoding='utf-8') as read_file:
+    blog_posts = json.load(read_file)
 
 
 @app.route('/')
 def index():
     return render_template('index.html', posts=blog_posts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+
+        new_post = {
+            "id": len(blog_posts) + 1,
+            "author": request.form.get('Author'),
+            "title": request.form.get('Title'),
+            "content": request.form.get('Content')
+        }
+        blog_posts.append(new_post)
+
+        with open('blog_posts.json', 'w', encoding='utf-8') as write_file:
+            json.dump(blog_posts, write_file, indent=2)
+
+        return redirect(url_for('index'))
+
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
